5e5eb6a571c6780c8de3b6241a3782b19ab57df0
Muhsin <muhsintt777@gmail.com>
2024-11-27 20:29:18 +0530

    feat : create login screen component

---
+++ src/features/auth/login-screen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export const LoginScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});

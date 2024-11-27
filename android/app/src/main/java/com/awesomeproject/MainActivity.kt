226b0ce3ab87bb1a9422b2ab97563d0317111c50
Muhsin <muhsintt777@gmail.com>
2024-11-27 20:27:05 +0530

    chore : add deps react navigations

--- android/app/src/main/java/com/awesomeproject/MainActivity.kt
+++ android/app/src/main/java/com/awesomeproject/MainActivity.kt
@@ -5,7 +5,17 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+// custom-changes
+// react navigation
+import android.os.Bundle;
+
 class MainActivity : ReactActivity() {
+
+  // custom-changes
+  // react navigation
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
